df95c2f02ad6e29660aae42bb9f63282f09abf46
suna-stransa <yuta.sunadoi@stransa.co.jp>
2020-06-16 09:24:56 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+
+import { withAuthenticator } from 'aws-amplify-react'; // ログイン画面のモジュールの読み込み
+import Amplify, { Auth } from 'aws-amplify';           // amplifyのモジュールの読み込み
+import aws_exports from './aws-exports';               // 設定情報を読み込みます。
+Amplify.configure(aws_exports);                        // 設定情報をAmplifyに反映させます。
+
 function App() {
+  // ログアウトボタンの準備
+  function signOut() {
+    Auth.signOut()
+  }
+
+  useEffect(() => {
+    const response = axios.get("https://rzl4a40l77.execute-api.ap-northeast-1.amazonaws.com/demo?TestId=1");
+    console.log(response);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit src/App.js and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,9 +35,9 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={ () => signOut() }>ログアウト</button>
       </header>
     </div>
   );
 }
-
-export default App;
+export default withAuthenticator(App);
